0d92cecfb12392a34f21a53d633a7d985d8bccc0
Conexion <Conexion@uniroca.locall>
2024-09-13 16:00:44 -0500

    commit ultimo desde ofi

--- PythonPy4JClient/src/test_connection/Py4JClient.py
+++ PythonPy4JClient/src/test_connection/Py4JClient.py
@@ -1,25 +1,25 @@
 
-from py4j.java_gateway import JavaGateway
+"""from py4j.java_gateway import JavaGateway
 
 #Crear la conexión en el servidor Java.
 gateway = JavaGateway()
 
 #Confirmación de conexión activa, de Python.
-print( "Test Desde Pyton" )
+print( "Test Desde Pyton" )"""
 
 """Invocando al método de Java de la clase JavaPy4JServer"""
-response = gateway.entry_point.messageTest()
-print( "Test, LLamada desde Python a: ", response )
+"""response = gateway.entry_point.messageTest()
+print( "Test, LLamada desde Python a: ", response )"""
 
 
 
 """MANEJO DE ERRORES, GENERADOS POR JAVA"""
 
-try:
+"""try:
     error_response = gateway.entry_point.induceError(1) #Prueba con el error 1
     print( f"Respuesta de Java: {error_response}" )
 except Exception as e:
-    print( f"Error capturado de Java: \n {e}" )
+    print( f"Error capturado de Java: \n {e}" )"""
 
 
 
